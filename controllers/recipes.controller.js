8d1f9b183cf2cb0d7e07ce59cabbd81e9003fe67
Jadde Suarez <jaddesuarez@gamail.com>
2022-12-10 16:10:16 +0100

    getcuisines

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -16,7 +16,7 @@ const getRecipeByCategory = (req, res, next) => {
     const { category } = req.query
 
     Recipe
-        .find({ category })
+        .find({ cuisines: { $elemMatch: category } })
         .then(response => res.json(response))
         .catch(err => next(err))
 }
@@ -33,22 +33,62 @@ const getRecipeByIngredients = (req, res, next) => {
 
 const createNewRecipe = (req, res, next) => {
 
-    const { title, readyInMinutes, servings, image, analyzedInstructions, cuisines, dishTypes, summary, extendedIngredients, owner } = req.body
+    const {
+        title,
+        readyInMinutes,
+        servings,
+        image,
+        analyzedInstructions,
+        cuisines,
+        dishTypes,
+        summary,
+        extendedIngredients } = req.body
 
     Recipe
-        .create({ title, readyInMinutes, servings, image, analyzedInstructions, cuisines, dishTypes, summary, extendedIngredients, owner })
+        .create({
+            title,
+            readyInMinutes,
+            servings,
+            image,
+            analyzedInstructions,
+            cuisines,
+            dishTypes,
+            summary,
+            extendedIngredients,
+            owner: req.payload._id
+        })
         .then(response => res.json(response))
         .catch(err => next(err))
 }
 
-const editRecipe = (req, res, next) => {
+const editRecipe = (req,
+    res, next) => {
 
     const { recipe_id } = req.params
 
-    const { title, readyInMinutes, servings, image, analyzedInstructions, cuisines, dishTypes, summary, extendedIngredients } = req.body
+    const {
+        title,
+        readyInMinutes,
+        servings,
+        image,
+        analyzedInstructions,
+        cuisines,
+        dishTypes,
+        summary,
+        extendedIngredients } = req.body
 
     Recipe
-        .findByIdAndUpdate(recipe_id, { title, readyInMinutes, servings, image, analyzedInstructions, cuisines, dishTypes, summary, extendedIngredients })
+        .findByIdAndUpdate(recipe_id, {
+            title,
+            readyInMinutes,
+            servings,
+            image,
+            analyzedInstructions,
+            cuisines,
+            dishTypes,
+            summary,
+            extendedIngredients
+        })
         .then(response => res.json(response))
         .catch(err => next(err))
 }
--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { isAuthenticated } = require("../middleware/jwt.middleware")
 
 const {
     getRecipeById,
@@ -9,14 +10,13 @@ const {
     editRecipe
 } = require('../controllers/recipes.controller')
 
-
 router.get('/:recipe_id/information', getRecipeById)
 
 router.get('/complexSearch', getRecipeByCategory)
 
 router.get('/findByIngredients', getRecipeByIngredients)
 
-router.post('/create', createNewRecipe)
+router.post('/create', isAuthenticated, createNewRecipe)
 
 router.post('/:recipe_id/edit', editRecipe)
 
